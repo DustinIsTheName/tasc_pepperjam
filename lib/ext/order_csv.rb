3a1b5a16e41b9b1e62ae2bf749e084bdf7e88c28
Dustin Milam <darklordsushi@yahoo.com>
2017-11-09 10:17:00 -0500

    checks for change before saving Order

--- lib/ext/order_csv.rb
+++ lib/ext/order_csv.rb
@@ -4,16 +4,30 @@ class Order_CSV
 		discount_codes = params["discount_codes"] ? params["discount_codes"].map{|x| x["code"]}.join('|') : ""
 
 	  params["line_items"].each do |line_item|
-	  	order = Order.new
-	  	order.program_id = "8671"
-			order.order_id = params["name"].gsub('#7000', '')
-			order.item_id = line_item["sku"]
-			order.item_price = line_item["price"].to_i * line_item["fulfillable_quantity"]
-			order.quantity = line_item["fulfillable_quantity"]
-			order.coupon = discount_codes
-			order.uploaded = false
+	  	fulfilled_amount = 0;
 
-			order.save
+	  	params["fulfillments"]&.each do |f|
+	  		l = f["line_items"].select{|i| i["id"] == line_item["id"]}.first
+	  		fulfilled_amount += l["quantity"] if l
+	  	end
+
+	  	old_order = Order.where(order_id: params["name"].gsub('#7000', ''), item_id: line_item["sku"]).last
+	  	puts Colorize.orange(old_order&.quantity)
+	  	puts Colorize.purple(line_item["fulfillable_quantity"] + fulfilled_amount)
+	  	if old_order.nil? or old_order.quantity != line_item["fulfillable_quantity"] + fulfilled_amount
+
+		  	order = Order.new
+		  	order.program_id = "8671"
+				order.order_id = params["name"].gsub('#7000', '')
+				order.item_id = line_item["sku"]
+				order.item_price = line_item["price"].to_i * (line_item["fulfillable_quantity"] + fulfilled_amount)
+				order.quantity = line_item["fulfillable_quantity"] + fulfilled_amount
+				order.coupon = discount_codes
+				order.uploaded = false
+
+				order.save
+
+			end
 	  end
 	end
 
