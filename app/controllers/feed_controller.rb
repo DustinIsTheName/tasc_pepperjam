619646c9563f0615ad18f34201e564caa5e58c42
Dustin Milam <darklordsushi@yahoo.com>
2019-04-30 16:31:29 -0400

    remove <head> and <body> tags from feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,8 +1,14 @@
 class FeedController < ApplicationController
 	layout false
 
-	def collection
+	def collection   
 		@response = open("https://www.tascperformance.com/collections/xml-google-shopping-feed-all").read
+
+    @response = @response.gsub(/<head>(.|\n)*<\/head>/, '')
+    @response = @response.gsub('<body>', '')
+    @response = @response.gsub('</body>', '')
+
+    puts @response
 	end
 
 end
