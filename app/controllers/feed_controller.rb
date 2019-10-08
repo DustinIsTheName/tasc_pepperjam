869a25a2b59fa25ae2263145928ff01223aad00a
Dustin Milam <darklordsushi@yahoo.com>
2019-10-08 14:49:21 -0400

    restructured feed controller and view

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,14 +1,17 @@
 class FeedController < ApplicationController
 	layout false
 
-	def collection   
-		@response = open("https://www.tascperformance.com/collections/xml-google-shopping-feed-all").read
+	def collection
+  	# @response = open("https://www.tascperformance.com/collections/xml-google-shopping-feed-all").read
 
-    @response = @response.gsub(/<head>(.|\n)*<\/head>/, '')
-    @response = @response.gsub('<body>', '')
-    @response = @response.gsub('</body>', '')
+   #  @response = @response.gsub(/<head>(.|\n)*<\/head>/, '')
+   #  @response = @response.gsub('<body>', '')
+   #  @response = @response.gsub('</body>', '')
 
-    puts @response
+    @shop = Shop.first
+    @collection = Collection.first
+
+    @products = Product.all
 	end
 
 end
